84ee0eb2fc09f4d243cc5f55cbf0a94fb801b9e1
Takaya-Sugino <takaya.sugino@kellogg.northwestern.edu>
2022-02-20 21:57:08 +0000

    change

---
+++ app/controllers/posts_controller.rb
@@ -0,0 +1,19 @@
+class PostsController < ApplicationController
+    def index
+        @posts = Post.all 
+     end 
+ 
+     def show
+         @posts = Post.find(params["id"])
+      end 
+ 
+      def new
+         @post = Post.new
+      end
+ 
+      def create
+         @post = Post.new(params["post"])
+         @post.save
+         redirect_to "/posts"
+      end
+end
